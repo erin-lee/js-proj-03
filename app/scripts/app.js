b52d3434614005ac130847c27810a667f3b77d71
Erin L <erinlee2007@u.northwestern.edu>
2014-02-17 22:33:13 -0500

    github api

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,3 +4,38 @@ define([], function () {
 
     return '\'Allo \'Allo!';
 });
+
+var user = 'erin-lee'
+
+var getUser = function(resp) {
+  var data = resp.data;
+  console.log(data);
+  var avatar = data.avatar_url;
+  var username = data.login;
+  var email = data.email;
+  var link = data.html_url;
+  console.log(avatar);
+  $('#js-username').html(username);
+  $('#js-email').html(email);
+  $('#js-link').html([
+    '<a href="',
+      link,
+      '" target="_blank">',
+      link,
+      '</a>'
+    ].join('')
+  );
+  $('#js-avatar').append([
+        '<img class="avatar" src="',
+        avatar,
+        '">'
+      ].join('')
+    )
+}
+
+$.ajax({
+    url: "https://api.github.com/users/" + user,
+    dataType: 'jsonp',
+    success: getUser
+  }
+);
